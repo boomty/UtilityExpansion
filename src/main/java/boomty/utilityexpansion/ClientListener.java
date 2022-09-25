479c95fef4ef59ea25a7418e6ab71aca9d1b603f
Booomty <114364342+Booomty@users.noreply.github.com>
2022-09-25 14:22:00 -0700

    Add files via upload

---
+++ src/main/java/boomty/utilityexpansion/ClientListener.java
@@ -0,0 +1,22 @@
+package boomty.utilityexpansion;
+
+import boomty.utilityexpansion.client.renderer.armor.RomanArmorRenderer;
+import boomty.utilityexpansion.item.RomanArmorItem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
+
+@Mod.EventBusSubscriber(modid = utilityexpansion.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientListener {
+
+    @SubscribeEvent
+    public static void registerArmorRenderers(final EntityRenderersEvent.AddLayers event) {
+        if (!FMLEnvironment.production && !utilityexpansion.DISABLE_IN_DEV) {
+            GeoArmorRenderer.registerArmorRenderer(RomanArmorItem.class, () -> new RomanArmorRenderer());
+        }
+    }
+
+}
