278adf00822db78291068e256f2f84ed40f00d96
boomty <114364342+boomty@users.noreply.github.com>
2022-10-03 21:06:45 -0700

    Testing Item Added

---
+++ src/main/java/boomty/utilityexpansion/item/TestingItem.java
@@ -0,0 +1,55 @@
+package boomty.utilityexpansion.item;
+
+import boomty.utilityexpansion.client.renderer.armor.TestingItemRenderer;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
+import net.minecraftforge.client.IItemRenderProperties;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import java.util.function.Consumer;
+
+public class TestingItem extends SwordItem implements IAnimatable {
+    public AnimationFactory factory = new AnimationFactory(this);
+
+    public TestingItem(Tiers tiers, int attackdamage, float attackspeed, Properties builder) {
+        super(tiers, attackdamage, attackspeed, new Item.Properties());
+    }
+
+    @Override
+    public void initializeClient(Consumer<IItemRenderProperties> consumer) {
+        super.initializeClient(consumer);
+        consumer.accept(new IItemRenderProperties() {
+            private final BlockEntityWithoutLevelRenderer renderer = new TestingItemRenderer();
+
+            @Override
+            public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
+                return renderer;
+            }
+        });
+    }
+
+    @Override
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController(this, "controller",
+                0, this::predicate));
+    }
+
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
+
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return this.factory;
+    }
+}
