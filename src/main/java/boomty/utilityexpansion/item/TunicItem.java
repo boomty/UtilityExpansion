05fffe3ed0d8297ea7be1c40421f0de5a72f9891
Forstandan <114364542+Forstandan@users.noreply.github.com>
2022-10-26 22:39:09 -0700

    Update TunicItem.java

--- src/main/java/boomty/utilityexpansion/item/TunicItem.java
+++ src/main/java/boomty/utilityexpansion/item/TunicItem.java
@@ -1,23 +1,8 @@
 package boomty.utilityexpansion.item;
 
-import boomty.utilityexpansion.client.renderer.armor.TunicItemRenderer;
-import net.minecraft.commands.arguments.item.ItemInput;
 import boomty.utilityexpansion.utilityexpansion;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.Level;
-import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -52,54 +37,54 @@ public class TunicItem extends GeoArmorItem implements IAnimatable {
         return PlayState.CONTINUE;
     }
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, utilityexpansion.MOD_ID);
-
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level p_40395_, Player p_40396_, InteractionHand p_40397_) {
-
-        //second solution
-//        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(utilityexpansion.MOD_ID, "geo/tunic_item.geo.json"));
-//        ItemStack itemstack2 = new ItemStack(item);
-
-        //Sets itemstack equal to the item currently held by player "p_40396_"
-        ItemStack itemstack = p_40396_.getItemInHand(p_40397_);
-        //creates a new tunicItem with the type Item
-        Item tunicItem = new TunicItem(ArmorMaterials.LEATHER, EquipmentSlot.LEGS, new Item.Properties());
-        //Uses ItemStack constructor to create a new ItemStack with an Item
-        ItemStack itemstack2 = new ItemStack(tunicItem);
-        //Finds the equipment slot designated to a chestplate (chest slot)
-        EquipmentSlot equipmentslot = Mob.getEquipmentSlotForItem(itemstack);
-        //
-        ItemStack itemstack3 = p_40396_.getItemBySlot(equipmentslot);
-        ItemStack itemstack4 = p_40396_.getItemBySlot(equipmentslot.LEGS);
-
-        //Checks if chestplate and legging slot is empty
-        if (itemstack3.isEmpty() && itemstack4.isEmpty())
-        {
-            p_40396_.setItemSlot(equipmentslot, itemstack.copy());
-            //Put "tunic legging" into leg slot
-            //Make sure to change itemstack.copy()
-            p_40396_.setItemSlot(equipmentslot.LEGS, itemstack2.copy());
-
-            if (!p_40395_.isClientSide())
-            {
-                p_40396_.awardStat(Stats.ITEM_USED.get(this));
-            }
-            itemstack.setCount(0);
-            return InteractionResultHolder.sidedSuccess(itemstack, p_40395_.isClientSide());
-        }
-        else
-        {
-            return InteractionResultHolder.fail(itemstack);
-        }
-    }
-
-    @SubscribeEvent
-    public static void registerRenderers(final EntityRenderersEvent.AddLayers event) {
-        GeoArmorRenderer.registerArmorRenderer(TunicItem.class, () -> new TunicItemRenderer());
-    }
-
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+//    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, utilityexpansion.MOD_ID);
+//
+//    @Override
+//    public InteractionResultHolder<ItemStack> use(Level p_40395_, Player p_40396_, InteractionHand p_40397_) {
+//
+//        //second solution
+////        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(utilityexpansion.MOD_ID, "geo/tunic_item.geo.json"));
+////        ItemStack itemstack2 = new ItemStack(item);
+//
+//        //Sets itemstack equal to the item currently held by player "p_40396_"
+//        ItemStack itemstack = p_40396_.getItemInHand(p_40397_);
+//        //creates a new tunicItem with the type Item
+//        Item tunicItem = new TunicItem(ArmorMaterials.LEATHER, EquipmentSlot.LEGS, new Item.Properties());
+//        //Uses ItemStack constructor to create a new ItemStack with an Item
+//        ItemStack itemstack2 = new ItemStack(tunicItem);
+//        //Finds the equipment slot designated to a chestplate (chest slot)
+//        EquipmentSlot equipmentslot = Mob.getEquipmentSlotForItem(itemstack);
+//        //
+//        ItemStack itemstack3 = p_40396_.getItemBySlot(equipmentslot);
+//        ItemStack itemstack4 = p_40396_.getItemBySlot(equipmentslot.LEGS);
+//
+//        //Checks if chestplate and legging slot is empty
+//        if (itemstack3.isEmpty() && itemstack4.isEmpty())
+//        {
+//            p_40396_.setItemSlot(equipmentslot, itemstack.copy());
+//            //Put "tunic legging" into leg slot
+//            //Make sure to change itemstack.copy()
+//            p_40396_.setItemSlot(equipmentslot.LEGS, itemstack2.copy());
+//
+//            if (!p_40395_.isClientSide())
+//            {
+//                p_40396_.awardStat(Stats.ITEM_USED.get(this));
+//            }
+//            itemstack.setCount(0);
+//            return InteractionResultHolder.sidedSuccess(itemstack, p_40395_.isClientSide());
+//        }
+//        else
+//        {
+//            return InteractionResultHolder.fail(itemstack);
+//        }
+//    }
+//
+//    @SubscribeEvent
+//    public static void registerRenderers(final EntityRenderersEvent.AddLayers event) {
+//        GeoArmorRenderer.registerArmorRenderer(TunicItem.class, () -> new TunicItemRenderer());
+//    }
+//
+//    public static void register(IEventBus eventBus) {
+//        ITEMS.register(eventBus);
+//    }
 }
