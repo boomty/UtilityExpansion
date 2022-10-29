8e277279dd9d3b962aef328977f0523c010230a9
Forstandan <114364542+Forstandan@users.noreply.github.com>
2022-10-29 00:56:17 -0700

    Update ModEvents.java

--- src/main/java/boomty/utilityexpansion/Events/ModEvents.java
+++ src/main/java/boomty/utilityexpansion/Events/ModEvents.java
@@ -23,19 +23,14 @@ public class ModEvents {
     {
         if (event.getEntity() instanceof Player player)
         {
-            Item TunicLegsItem = ItemRegistry.tunic_legs.get();
-            ItemStack TunicLegs = new ItemStack(TunicLegsItem);
+            Item tunicItem = ItemRegistry.tunic.get();
+            Item tunicLegsItem = ItemRegistry.tunic_legs.get();
 
-            Item TunicItem = ItemRegistry.tunic.get();
-            ItemStack Tunic = new ItemStack(TunicItem);
-
-            if (event.getTo() == Tunic) {
-                //equip chestpiece onto chest slot
-                player.setItemSlot(event.getSlot(), player.getItemInHand(InteractionHand.MAIN_HAND));
+            if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() == tunicItem) {
                 //equip lower part on to leg slot
-                player.setItemSlot(EquipmentSlot.LEGS, TunicLegs);
+                player.setItemSlot(EquipmentSlot.LEGS, new ItemStack(tunicLegsItem));
             }
-            else if (player.hasItemInSlot(EquipmentSlot.LEGS) && player.getItemBySlot(EquipmentSlot.LEGS).equals(TunicLegs))
+            else if (player.hasItemInSlot(EquipmentSlot.LEGS) && player.getItemBySlot(EquipmentSlot.LEGS).getItem() == tunicLegsItem)
             {
                 player.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
             }
@@ -45,71 +40,68 @@ public class ModEvents {
     @SubscribeEvent
     public static void equipLorica(LivingEquipmentChangeEvent event)
     {
-        if (event.getEntity() instanceof Player) {
-            Item LoricaLegs = ItemRegistry.lorica_legs.get();
-            Item LoricaSegmentata = ItemRegistry.lorica_segmentata.get();
-            //Initialize player object
-            Player player = (Player) event.getEntity();
+        if (event.getEntity() instanceof Player player)
+        {
+            Item loricaLegs = ItemRegistry.lorica_legs.get();
+            Item loricaSegmentata = ItemRegistry.lorica_segmentata.get();
+
             //Check if the chest slot and leg slot are empty
-            if (player.getItemBySlot(event.getSlot()).isEmpty() && player.getItemBySlot(EquipmentSlot.LEGS).isEmpty()) {
-                //equip chestpiece onto chest slot
-                player.setItemSlot(event.getSlot(), player.getItemInHand(InteractionHand.MAIN_HAND));
+            if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() == loricaSegmentata) {
                 //equip lower part on to leg slot
-                player.setItemSlot(EquipmentSlot.LEGS, new ItemStack(LoricaLegs));
+                player.setItemSlot(EquipmentSlot.LEGS, new ItemStack(loricaLegs));
             }
-            else if (player.getItemBySlot(EquipmentSlot.CHEST) == new ItemStack(LoricaSegmentata) && player.getItemBySlot(EquipmentSlot.LEGS) == new ItemStack(LoricaSegmentata))
+            else if (player.hasItemInSlot(EquipmentSlot.LEGS) && player.getItemBySlot(EquipmentSlot.LEGS).getItem() == loricaLegs)
             {
-                player.setItemSlot(EquipmentSlot.CHEST, ItemStack.EMPTY);
                 player.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
             }
         }
     }
 
-    @SubscribeEvent
-    public static void equipArmorStand(PlayerInteractEvent.EntityInteract event)
-    {
-        Entity entity = event.getTarget();
-        if (entity.getType() == EntityType.ARMOR_STAND)
-        {
-            //Declare itemstacks for the items to be checked for
-            ItemStack LoricaSegmentata = new ItemStack(ItemRegistry.lorica_segmentata.get());
-            ItemStack Tunic = new ItemStack(ItemRegistry.tunic.get());
-            //Check if the item that is being equipped is one of the declared itemstacks
-            if (event.getItemStack().equals(Tunic) || event.getItemStack().equals(LoricaSegmentata))
-            {
-                //Get the player that is interacting with the item
-                Player player = event.getPlayer();
-                //Get the interaction hand
-                InteractionHand hand = event.getHand();
-                //Create an itemstack for the item in hand
-                ItemStack item = player.getItemInHand(hand);
-                //Get the item's slot
-                EquipmentSlot slot = LivingEntity.getEquipmentSlotForItem(item);
-                //Create an itemstack for the slot
-                ItemStack slotItemStack = player.getItemBySlot(slot);
-                //Create an itemstack for the leg slot
-                ItemStack legSlot = player.getItemBySlot(EquipmentSlot.LEGS);
-                //Check if the itemstack that the user is trying to equip the armor is empty
-                if (slotItemStack.isEmpty() && legSlot.isEmpty())
-                {
-                    if (event.getItemStack() == LoricaSegmentata)
-                    {
-                        Item LoricaLegs = ItemRegistry.lorica_legs.get();
-                        //equip chestpiece onto chest slot
-                        entity.setItemSlot(slot, player.getItemInHand(InteractionHand.MAIN_HAND));
-                        //equip lower part on to leg slot
-                        entity.setItemSlot(EquipmentSlot.LEGS, new ItemStack(LoricaLegs));
-                    }
-                    else if(event.getItemStack() == Tunic)
-                    {
-                        Item TunicLegs = ItemRegistry.tunic_legs.get();
-                        //equip chestpiece onto chest slot
-                        player.setItemSlot(slot, player.getItemInHand(InteractionHand.MAIN_HAND));
-                        //equip lower part on to leg slot
-                        player.setItemSlot(EquipmentSlot.LEGS, new ItemStack(TunicLegs));
-                    }
-                }
-            }
-        }
-    }
+//    @SubscribeEvent
+//    public static void equipArmorStand(PlayerInteractEvent.EntityInteract event)
+//    {
+//        Entity entity = event.getTarget();
+//        if (entity.getType() == EntityType.ARMOR_STAND)
+//        {
+//            //Declare itemstacks for the items to be checked for
+//            ItemStack LoricaSegmentata = new ItemStack(ItemRegistry.lorica_segmentata.get());
+//            ItemStack Tunic = new ItemStack(ItemRegistry.tunic.get());
+//            //Check if the item that is being equipped is one of the declared itemstacks
+//            if (event.getItemStack().equals(Tunic) || event.getItemStack().equals(LoricaSegmentata))
+//            {
+//                //Get the player that is interacting with the item
+//                Player player = event.getPlayer();
+//                //Get the interaction hand
+//                InteractionHand hand = event.getHand();
+//                //Create an itemstack for the item in hand
+//                ItemStack item = player.getItemInHand(hand);
+//                //Get the item's slot
+//                EquipmentSlot slot = LivingEntity.getEquipmentSlotForItem(item);
+//                //Create an itemstack for the slot
+//                ItemStack slotItemStack = player.getItemBySlot(slot);
+//                //Create an itemstack for the leg slot
+//                ItemStack legSlot = player.getItemBySlot(EquipmentSlot.LEGS);
+//                //Check if the itemstack that the user is trying to equip the armor is empty
+//                if (slotItemStack.isEmpty() && legSlot.isEmpty())
+//                {
+//                    if (event.getItemStack() == LoricaSegmentata)
+//                    {
+//                        Item LoricaLegs = ItemRegistry.lorica_legs.get();
+//                        //equip chestpiece onto chest slot
+//                        entity.setItemSlot(slot, player.getItemInHand(InteractionHand.MAIN_HAND));
+//                        //equip lower part on to leg slot
+//                        entity.setItemSlot(EquipmentSlot.LEGS, new ItemStack(LoricaLegs));
+//                    }
+//                    else if(event.getItemStack() == Tunic)
+//                    {
+//                        Item TunicLegs = ItemRegistry.tunicLegs.get();
+//                        //equip chestpiece onto chest slot
+//                        player.setItemSlot(slot, player.getItemInHand(InteractionHand.MAIN_HAND));
+//                        //equip lower part on to leg slot
+//                        player.setItemSlot(EquipmentSlot.LEGS, new ItemStack(TunicLegs));
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
