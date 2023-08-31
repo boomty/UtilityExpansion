9d073f1a767b4cf6535916fe97a42bba1744e6ea
Forstandan <114364542+Forstandan@users.noreply.github.com>
2023-08-31 12:17:45 -0700

    Update AbstractContainerScreenMixin.java

--- src/main/java/boomty/utilityexpansion/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/boomty/utilityexpansion/mixin/AbstractContainerScreenMixin.java
@@ -44,29 +44,42 @@ public abstract class AbstractContainerScreenMixin <T extends AbstractContainerM
 //            this.minecraft.gameMode.handleInventoryMouseClick(this.getMenu().containerId, p_97779_, p_97780_, p_97781_, this.minecraft.player);
 //    }
 
-    // user cannot remove the lorica_legs
+    // method for equipping and unequipping corresponding armor 
     @Inject(method = "slotClicked", at = @At(value = "HEAD"), cancellable = true)
     public void slotClicked(Slot p_97778_, int p_97779_, int p_97780_, ClickType p_97781_, CallbackInfo ci){
-        if (p_97778_ != null){
+        ModItems modItems = new ModItems();
+        Hashtable<String, String> itemMap = modItems.getItemMap();
+        Hashtable<String, ItemStack> correspondingItemStack = modItems.getCorrespondingItemStack();
+
+        AbstractContainerScreen<T> instance = ((AbstractContainerScreen<T>) (Object) this);
+        if (p_97778_ != null) {
+            String slotItem = p_97778_.getItem().getItem().getDescriptionId();
+            ItemStack itemStack = correspondingItemStack.get(itemMap.get(instance.getMenu().getCarried().getDescriptionId()));
+
             // prevent player from removing leg portion
-            if (ItemStack.matches(p_97778_.getItem(), lorica_legs)) {
+            // ItemStack.matches(p_97778_.getItem(), lorica_legs)
+            if (itemMap.containsValue(slotItem)) {
                 ci.cancel();
             }
             // if player is manually dragging item into armor slot
-            else if (p_97779_ == 6 && ItemStack.matches(((AbstractContainerScreen<T>) (Object) this).getMenu().getCarried(), lorica_segmentata)) {
+            // p_97779_ == 6 && ItemStack.matches(instance.getMenu().getCarried(), lorica_segmentata)
+            else if (p_97779_ == 6 && itemMap.containsKey(instance.getMenu().getCarried().getDescriptionId())) {
                 // need to check to make sure there are at least two empty slots in inventory
-                ((AbstractContainerScreen<T>) (Object) this).getMinecraft().player.setItemSlot(EquipmentSlot.LEGS, lorica_legs);
+                instance.getMinecraft().player.setItemSlot(EquipmentSlot.LEGS, correspondingItemStack.get(itemMap.get(instance.getMenu().getCarried().getDescriptionId())));
             }
             // if player shift clicks item into slot
-            else if (p_97781_ == ClickType.QUICK_MOVE && ItemStack.matches(p_97778_.getItem(), lorica_segmentata) && p_97779_ != 6) {
-                ((AbstractContainerScreen<T>) (Object) this).getMinecraft().player.setItemSlot(EquipmentSlot.LEGS, lorica_legs);
+            // p_97781_ == ClickType.QUICK_MOVE && ItemStack.matches(p_97778_.getItem(), lorica_segmentata) && p_97779_ != 6
+            else if (p_97781_ == ClickType.QUICK_MOVE && itemMap.containsKey(slotItem) && p_97779_ != 6) {
+                ((AbstractContainerScreen<T>) (Object) this).getMinecraft().player.setItemSlot(EquipmentSlot.LEGS, correspondingItemStack.get(itemMap.get(slotItem)));
             }
             // if player removes chestplate by pressing hotbar key
-            else if (p_97779_ == 6 && p_97781_ == ClickType.SWAP && ItemStack.matches(p_97778_.getItem(), lorica_segmentata)) {
+            // p_97779_ == 6 && p_97781_ == ClickType.SWAP && ItemStack.matches(p_97778_.getItem(), lorica_segmentata)
+            else if (p_97779_ == 6 && p_97781_ == ClickType.SWAP && itemMap.containsKey(slotItem)) {
                 ((AbstractContainerScreen<T>) (Object) this).getMinecraft().player.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
             }
             // if player removes chestplate by clicking
-            else if (p_97779_ == 6 && ItemStack.matches(p_97778_.getItem(), lorica_segmentata)) {
+            // p_97779_ == 6 && ItemStack.matches(p_97778_.getItem(), lorica_segmentata)
+            else if (p_97779_ == 6 && itemMap.containsKey(slotItem)) {
                 ((AbstractContainerScreen<T>) (Object) this).getMinecraft().player.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
             }
         }
