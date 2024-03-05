9f5554718c7b85093b17d7a36355837d1c1b1053
Forstandan <114364542+Forstandan@users.noreply.github.com>
2024-03-04 21:13:01 -0800

    Fixed issue with leg armor not persisting between sessions. New errors with mixin logic need to be fixed

--- src/main/java/boomty/utilityexpansion/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/boomty/utilityexpansion/mixin/AbstractContainerScreenMixin.java
@@ -71,94 +71,36 @@ public abstract class AbstractContainerScreenMixin <T extends AbstractContainerM
             // if player is manually dragging item into armor slot
             // p_97779_ == 6 && ItemStack.matches(instance.getMenu().getCarried(), lorica_segmentata)
             else if (p_97779_ == 6 && correspondingItems.containsKey(instance.getMenu().getCarried().getItem())) {
-                ItemStack carryItem = instance.getMenu().getCarried();
-                setSlots(player, carryItem, new ItemStack(correspondingItems.get(carryItem.getItem())),
-                        player.getItemBySlot(EquipmentSlot.LEGS), correspondingItems, p_97779_, p_97780_, p_97781_,
-                        player.getInventory().armor);
+                Item carryItem = instance.getMenu().getCarried().getItem();
+                int legSlotId = 7;
+                int mouseButtonNum = 0;
+                player.getInventory().armor.set(EquipmentSlot.LEGS.getIndex(), new ItemStack(correspondingItems.get(carryItem)));
+                gamemode.handleInventoryMouseClick(instance.getMenu().containerId, legSlotId, mouseButtonNum, ClickType.PICKUP, player);
+
             }
             // if player shift clicks item into slot, ensure that the chest slot is not taken
             // p_97781_ == ClickType.QUICK_MOVE && ItemStack.matches(p_97778_.getItem(), lorica_segmentata) && p_97779_ != 6
             else if (p_97781_ == ClickType.QUICK_MOVE && correspondingItems.containsKey(slotItem) && p_97779_ != 6
                     && Objects.requireNonNull(player).getItemBySlot(EquipmentSlot.CHEST).isEmpty()) {
-                setSlots(player, new ItemStack(slotItem), new ItemStack(correspondingItems.get(slotItem)),
-                        player.getItemBySlot(EquipmentSlot.LEGS), correspondingItems, p_97779_, p_97780_, p_97781_,
-                        player.getInventory().armor);
+                int legSlotId = 7;
+                int mouseButtonNum = 0;
+                player.getInventory().armor.set(EquipmentSlot.LEGS.getIndex(), new ItemStack(correspondingItems.get(slotItem)));
+                gamemode.handleInventoryMouseClick(instance.getMenu().containerId, legSlotId, mouseButtonNum, ClickType.PICKUP, player);
             }
             // if player removes chestplate by pressing hotbar key
             // p_97779_ == 6 && p_97781_ == ClickType.SWAP && ItemStack.matches(p_97778_.getItem(), lorica_segmentata)
             else if (p_97779_ == 6 && p_97781_ == ClickType.SWAP && correspondingItems.containsKey(slotItem)) {
-//                player.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
                 player.getInventory().armor.set(EquipmentSlot.LEGS.getIndex(), ItemStack.EMPTY);
-//                sendPackets(player, this.getMenu().containerId, p_97779_, p_97780_, p_97781_, player.containerMenu);
             }
             // if player removes chestplate by clicking
             // p_97779_ == 6 && ItemStack.matches(p_97778_.getItem(), lorica_segmentata)
             else if (p_97779_ == 6 && correspondingItems.containsKey(slotItem)) {
-//                player.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
                 player.getInventory().armor.set(EquipmentSlot.LEGS.getIndex(), ItemStack.EMPTY);
-//                sendPackets(player, this.getMenu().containerId, p_97779_, p_97780_, p_97781_, player.containerMenu);
             }
         }
     }
 
-    private void setSlots(Player player, ItemStack mainComponent, ItemStack correspondingComponent, ItemStack currentItem,
-                          Map<Item, Item> correspondingItemStack, int slotNum, int buttonNum, ClickType clickType,
-                          NonNullList<ItemStack> armorSlots) {
-        // if player has something already in their leg slot, make sure there is at least one slot empty
-        if (numOfEmptySlots() >= 1 && player.getItemBySlot(EquipmentSlot.LEGS) != ItemStack.EMPTY) {
-            // set leg slot with corresponding item
-//            player.setItemSlot(EquipmentSlot.LEGS, correspondingComponent);
-            player.getInventory().armor.set(EquipmentSlot.LEGS.getIndex(), correspondingComponent);
-            // return saved item to player
-            if (!correspondingItemStack.containsKey(correspondingComponent.getItem())) {
-                player.addItem(currentItem);
-            }
-            sendPackets(player, this.getMenu().containerId, slotNum, buttonNum, clickType, player.containerMenu,
-                    armorSlots, mainComponent, correspondingComponent);
-        }
-        // if the player has nothing in their leg slot
-        else if (player.getItemBySlot(EquipmentSlot.LEGS) == ItemStack.EMPTY){
-//            player.setItemSlot(EquipmentSlot.LEGS, correspondingComponent);
-            player.getInventory().armor.set(EquipmentSlot.LEGS.getIndex(), correspondingComponent);
-//            sendPackets(player, this.getMenu().containerId, slotNum, buttonNum, clickType, player.containerMenu,
-//                    armorSlots, mainComponent, correspondingComponent);
-        }
-    }
 
-    private void sendPackets(Player player, int windowId, int slotNum, int buttonNum, ClickType clickType,
-                             AbstractContainerMenu menu, NonNullList<ItemStack> armorSlots, ItemStack component1,
-                             ItemStack component2) {
-        NonNullList<Slot> nonnulllist = menu.slots;
-        int i = nonnulllist.size();
-        List<ItemStack> list = Lists.newArrayListWithCapacity(i);
-
-        for(Slot slot : nonnulllist) {
-            list.add(slot.getItem().copy());
-        }
-
-        Int2ObjectMap<ItemStack> int2objectmap = new Int2ObjectOpenHashMap<>();
-
-        int2objectmap.put(6, component1);
-        int2objectmap.put(7, component2);
-
-        if (player instanceof ServerPlayer) {
-            ServerPlayer serverPlayer = (ServerPlayer) player;
-            serverPlayer.connection.send(new ServerboundContainerClickPacket(windowId, menu.getStateId(), slotNum, buttonNum, clickType, menu.getCarried().copy(), int2objectmap));
-        }
-        else {
-            minecraft.getInstance().getConnection().send(new ServerboundContainerClickPacket(windowId, menu.getStateId(), slotNum, buttonNum, clickType, menu.getCarried().copy(), int2objectmap));
-        }
-
-        NonNullList<ItemStack> playerArmorList = player.getInventory().armor;
-        System.out.println(playerArmorList.size());
-
-        // Access the original player's inventory
-        for (ItemStack item : playerArmorList) {
-            if (!item.isEmpty()) {
-                System.out.println(item.getItem().getDescription().getString());
-            }
-        }
-    }
 
     public int numOfEmptySlots() {
         NonNullList<ItemStack> playerInventory =
